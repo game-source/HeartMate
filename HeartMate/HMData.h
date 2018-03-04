10c87954c0c2cb8c66a56be70b0efca7ad5586de
xaoxuu <xaoxuu@gmail.com>
2018-03-04 13:56:23 +0800

    rename

---
+++ HeartMate/HMData.h
@@ -0,0 +1,13 @@
+//
+//  HMData.h
+//  CardiotachMate
+//
+//  Created by xaoxuu on 04/03/2018.
+//  Copyright © 2018 xaoxuu. All rights reserved.
+//
+
+#import <Realm/Realm.h>
+
+@interface HMData : RLMObject
+
+@end
---
+++ HeartMate/HMData.m
@@ -0,0 +1,13 @@
+//
+//  HMData.m
+//  CardiotachMate
+//
+//  Created by xaoxuu on 04/03/2018.
+//  Copyright © 2018 xaoxuu. All rights reserved.
+//
+
+#import "HMData.h"
+
+@implementation HMData
+
+@end
